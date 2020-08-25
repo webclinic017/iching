242785a14f41f94260a6c0c7923cf4a4d795dad7
yt7589 <yt7589@qq.com>
2020-08-25 20:57:38 +0800

    add agent, action definition, choose action method

---
+++ apps/sop/sop_agent.py
@@ -0,0 +1,33 @@
+# 强化学习中的Agent，负责与环境的交互，同时调用对应的策略，生成适合的
+# 行动
+import numpy as np
+
+class SopAgent(object):
+    IDX_OPTION = 0
+    IDX_ACTION = 1
+    IDX_PERCENT = 2
+
+    def __init__(self, env):
+        self.refl = 'apps.sop.Agent'
+        self.reset(env)
+
+    def reset(self, env):
+        self.action = [
+            np.zeros((len(env.ds.key_list),)),
+            np.zeros((3,)),
+            np.zeros((10,))
+        ]
+
+    def choose_action(self, obs, reward):
+        '''
+        根据环境当前状态选择本时间点的行动，将上一时间点行动的奖励信号
+        用于策略学习
+        '''
+        self.action[self.action > 0] = 0
+        option_idx = 2
+        action_idx = 1
+        percent_idx = 2
+        self.action[SopAgent.IDX_OPTION][option_idx] = 1
+        self.action[SopAgent.IDX_ACTION][action_idx] = 1
+        self.action[SopAgent.IDX_PERCENT][percent_idx] = 1
+        return self.action
--- apps/sop/sop_env.py
+++ apps/sop/sop_env.py
@@ -3,12 +3,16 @@ import numpy as np
 import gym
 from gym import spaces
 # 
+from apps.sop.sop_agent import SopAgent
 from apps.sop.ds.sh50etf_dataset import Sh50etfDataset
 
 class SopEnv(gym.Env):
     def __init__(self):
         self.refl = ''
         self.tick = 0
+        # action为3维数组：1维-是期权合约编号；2维-买入持有卖出；
+        # 3维-百分比，缺省为100%
+        self.agent = SopAgent()
 
     def startup(self, args={}):
         self.ds = Sh50etfDataset()
@@ -16,7 +20,7 @@ class SopEnv(gym.Env):
         obs, reward, done, info = self._next_observation(), 0, False, {}
         for dt in self.ds.dates:
             print('{0}: '.format(dt))
-            action = {}
+            action = self.agent.choose_action(obs, reward)
             obs, reward, done, info = self.step(action)
             X = obs['X'].cpu().numpy()
             y = obs['y'].cpu().numpy()
@@ -28,6 +32,7 @@ class SopEnv(gym.Env):
 
     def reset(self):
         print('重置环境到初始状态')
+        self.agent.reset()
         self.tick = 0
 
     def _next_observation(self):

f3b2b42cae2099c79cfdba98a1b4d0660380a51b
yt7589 <yt7589@qq.com>
2020-08-27 17:13:14 +0800

    test calculation of security deposit of put option

--- uts/apps/sop/exchange/t_commission.py
+++ uts/apps/sop/exchange/t_commission.py
@@ -17,7 +17,10 @@ class TCommission(unittest.TestCase):
         royalty = cmn.calculate_royalty(0.1, 28)
         self.assertTrue(abs(royalty - 28000))
 
-    def test_calculate_security_deposit(self):
+    def test_calculate_security_deposit1(self):
+        '''
+        测试认购期权保证金计算
+        '''
         comm = Commission()
         oc = OptionContract()
         oc.exercise_price = 22.0
@@ -25,6 +28,21 @@ class TCommission(unittest.TestCase):
         oc.option_contract_type = OptionContract.OCT_CALL
         oc.side = OptionContract.SIDE_SHORT
         asset_price = 21.5
+        quant = 10
+        amount = comm.calculate_security_deposit(oc, asset_price, quant)
+        print(amount)
+
+    def test_calculate_security_deposit2(self):
+        '''
+        测试认沽期权保证金计算
+        '''
+        comm = Commission()
+        oc = OptionContract()
+        oc.exercise_price = 22.0
+        oc.price = 0.72
+        asset_price = 21.50 # 标的收盘价
+        oc.option_contract_type = OptionContract.OCT_PUT
+        oc.side = OptionContract.SIDE_SHORT
         quant = 10
         amount = comm.calculate_security_deposit(oc, asset_price, quant)
         print(amount)

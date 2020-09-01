3993f1061b69e22d7e52f4c567bf0920c2bbf96f
yt7589 <yt7589@qq.com>
2020-09-01 14:26:20 +0800

    get each option contract quotation info

--- apps/sop/snp/base_strategy.py
+++ apps/sop/snp/base_strategy.py
@@ -13,17 +13,17 @@ class BaseStrategy(object):
         print('策略类看到的环境状态：{0};'.format(obs['X'].shape))
         option_codes = SopRegistry.get(SopRegistry.K_OPTION_CODES)
         oc = option_codes[0]
-        print('oc: {0};'.format(option_codes[0]))
         oc_quotations = []
         asset_quotations = []
         for day_idx in range(len(option_codes)):
-            daily_quotation = obs['X'][day_idx*373 : (day_idx+1)*373]
+            daily_quotation = [ x.item() for x in obs['X'][day_idx*373 : (day_idx+1)*373]]
             oc_idx = SopRegistry.get(SopRegistry.K_OC_TO_IDX)[oc]
             oc_quotations += daily_quotation[oc_idx*8 : (oc_idx+1)*8]
             asset_quotations += daily_quotation[-5:]
+        print('oc: {0};'.format(oc))
         print('oc_quotations: {0};'.format(oc_quotations))
+        print('asset_quotations: {0};'.format(asset_quotations))
         # 获取期权行情数据
-        sys.exit(0)
         self.action.reset()
         option_idx = 0
         self.action.action[SopAction.IDX_OPTION][option_idx] = 1

c6479f14d9a617d9731829146e2b501f77aa6386
yantao <yt7589@qq.com>
2020-12-15 21:38:03 +0800

    update the ebook

--- apps/drl/chp001/exp001_001.py
+++ apps/drl/chp001/exp001_001.py
@@ -1,11 +1,12 @@
 #
 
-class Exp02001(object):
+class Exp001001(object):
     def __init__(self):
         self.refl = ''
 
     def startup(self):
-        self.bandit_walk()
+        #self.bandit_walk()
+        self.bandit_slippery_walk()
 
     def bandit_walk(self):
         P = {
@@ -16,6 +17,23 @@ class Exp02001(object):
             1: {
                 0: [(1.0, 0, 0.0, True)],
                 1: [(1.0, 2, 1.0, True)]
+            },
+            2: {
+                0: [(1.0, 2, 0.0, True)],
+                1: [(1.0, 2, 0.0, True)]
+            }
+        }
+        print(P)
+
+    def bandit_slippery_walk(self):
+        P = {
+            0: {
+                0: [(1.0, 0, 0.0, True)],
+                1: [(1.0, 0, 0.0, True)]
+            },
+            1: {
+                0: [(0.8, 0, 0.0, True), (0.2, 2, 1.0, True)],
+                1: [(0.8, 2, 1.0, True), (0.2, 0, 0.0, True)]
             },
             2: {
                 0: [(1.0, 2, 0.0, True)],

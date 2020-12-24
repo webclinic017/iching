cfff68d5e5f1c42dbc6b5042a483c0ac13ccbafc
yantao <yt7589@qq.com>
2020-12-24 21:06:27 +0800

    add chapter 2

---
+++ apps/drl/chp002/exp002_001.py
@@ -0,0 +1,25 @@
+#
+import numpy as np
+import gym
+import gym_bandits
+
+class Exp002001(object):
+    def __init__(self):
+        self.refl = 'apps.drl.chp002.Exp002001'
+
+    def startup(self):
+        SEEDS = (12, 34, 56, 78, 90)
+        b2_Vs = []
+        for seed in SEEDS:
+            # env_name = 'BanditTwoArmedUniform-v0'
+            env_name = 'BanditTwoArmedDeterministicFixed-v0'
+            env = gym.make(env_name, seed=seed) ; env.reset()
+            b2_Q = np.array(env.env.p_dist * env.env.r_dist)
+            print('Two-Armed Bandit environment with seed', seed)
+            print('Probability of reward:', env.env.p_dist)
+            print('Reward:', env.env.r_dist)
+            print('Q(.):', b2_Q)
+            b2_Vs.append(np.max(b2_Q))
+            print('V*:', b2_Vs[-1])
+            print()
+        print('Mean V* across all seeds:', np.mean(b2_Vs))
--- apps/drl/drl_app.py
+++ apps/drl/drl_app.py
@@ -1,6 +1,7 @@
 #
 from apps.drl.chp001.exp001_001 import Exp001001
 from apps.drl.chp001.exp001_002 import Exp001002
+from apps.drl.chp002.exp002_001 import Exp002001
 
 class DrlApp(object):
     def __init__(self):
@@ -8,5 +9,6 @@ class DrlApp(object):
 
     def startup(self):
         #exp = Exp001001()
-        exp = Exp001002()
+        #exp = Exp001002()
+        exp = Exp002001()
         exp.startup()

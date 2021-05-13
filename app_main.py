72a8b045d143e428d77c686d841b9622f5a19714
yantao <yt7589@qq.com>
2021-05-13 10:19:41 +0800

    remove cache info

--- app_main.py
+++ app_main.py
@@ -1,4 +1,7 @@
 #
+from apps.nup_app import NupApp
+import sys
+import getopt
 import numpy as np
 '''
 from apps.ogml.ogml_app import OgmlApp
@@ -10,9 +13,11 @@ from apps.tcv.tcv_app import TcvApp
 from apps.fmml.fmml_app import FmmlApp
 from apps.wxs.wxs_app import WxsApp
 '''
+from apps.nup_app import NupApp
 from apps.sop.sop_app import SopApp
 from apps.ots.ots_app import OtsApp
 from apps.drl.drl_app import DrlApp
+#from iqt.iqt_app import IqtApp
 
 
 def norm_batch_tasks(batch_vals, task_num):
@@ -25,8 +30,25 @@ def norm_batch_tasks(batch_vals, task_num):
 def exp():
     pass
 
-def main():
-    print('易经量化交易系统 v0.0.1')
+params = {
+    'version': '0.0.1',
+    'mode': '1',
+    'exp': ''
+}
+
+def main(argv={}):
+    opts, args = getopt.getopt(argv, 'v:i', ['mode=', 'exp='])
+    print('args:{0}; {1};'.format(type(args), args))
+    for opt, val in opts:
+        if opt=='-i':
+            print('易经量化交易平台')
+        elif opt=='-v':
+            params['version'] = val
+        elif opt=='--mode':
+            params['mode'] = val
+        elif opt=='--exp':
+            params['exp'] = val
+    print('mode={0}; exp={1};'.format(params['mode'], params['exp']))
     #app = OgmlApp()
     #app = TpApp()
     #app = RxgbApp()
@@ -35,9 +57,22 @@ def main():
     #app = TcvApp()
     #app = FmmlApp()
     #app = SopApp()
-    app = OtsApp()
-    #app = DrlApp()
+    if params['mode'] == 'ots':
+        app = OtsApp()
+    elif params['mode'] == 'drl':
+        app = DrlApp()
+    #elif params['mode'] == 'iqt':
+    #    app = IqtApp()
+    else:
+        app = NupApp()
     app.startup()
 
 if '__main__' == __name__:
-    main()
+    '''
+    命令行启动参数：python app_main.py -i -v 0.0.1 --mode iqt --exp tt a1 a2 a3
+      短选项：-字母 -字母:（接收参数） -t
+      长选项：--字符串
+      其他参数
+    '''
+    argv = sys.argv[1:]
+    main(argv)
---
+++ apps/nup_app.py
@@ -0,0 +1,8 @@
+#
+
+class NupApp(object):
+    def __init__(self):
+        self.name = 'apps.nup_app.NupApp'
+
+    def startup(args={}):
+        print('请指定要运行的应用')

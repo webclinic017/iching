dd1593ed7a27e10b8c8a905adb6ad4716b608367
yantao <yt7589@qq.com>
2021-05-30 19:42:28 +0800

    add version info to chapter 2 section 7

--- apps/mml/c02/s07/c02_s07_e01.py
+++ apps/mml/c02/s07/c02_s07_e01.py
@@ -9,10 +9,18 @@ class C02S07E01(object):
         self.name = "apps.mml.c02.s07.c02_s07_e01.C02S07E01"
 
     def startup(self, args={}):
-        print('基变换矩阵')
+        print('基变换矩阵 v0.0.1')
         B = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-        B_tilde = self.generate_base_from_base(B)
-        print(B_tilde)
+        #B_tilde = self.generate_base_from_base(B)
+        #print(B_tilde)
+        C = np.array([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+        A_Phi = np.array([[1.0, 2.0, 0.0], [-1.0, -1.0, 3.0], [3.0, 7.0, 1.0], [-1.0, 2.0, 4.0]])
+        print('shape: {0}; \n{1};'.format(A_Phi.shape, A_Phi))
+        x = A_Phi[:,0]
+        print('x: shape: {0}; {1};'.format(x.shape, x))
+        b1 = np.matmul(A_Phi.T, x)
+        print('shape: {0}; {1};'.format(b1.shape, b1))
+
 
     def vector_linear_combination(self, base_v, low=-10.0, high=10.0):
         i_debug = 1
@@ -28,4 +36,4 @@ class C02S07E01(object):
         b_0 = self.vector_linear_combination(base_v)
         b_1 = self.vector_linear_combination(base_v)
         b_2 = self.vector_linear_combination(base_v)
-        return np.array([b_0, b_1, b_2])
+        return np.array([b_0, b_1, b_2]).T

14e0b43c9d60845695fae62c6f488df816c52769
yantao <yt7589@qq.com>
2021-06-22 10:36:56 +0800

    load stock minute bar gym environment

--- ann/dmrl/maml_trpo_engine.py
+++ ann/dmrl/maml_trpo_engine.py
@@ -1,8 +1,13 @@
 # 
 import os
 import json
+import pathlib
 import yaml
 import torch
+import gym
+# 
+from biz.drlt.ds.bar_data import BarData
+from biz.drlt.envs.minute_bar_env import MinuteBarEnv
 
 class MamlTrpoEngine(object):
     def __init__(self):
@@ -29,3 +34,44 @@ class MamlTrpoEngine(object):
         if config['seed'] is not None:
             torch.manual_seed(config['seed'])
             torch.cuda.manual_seed_all(config['seed'])
+        #env = gym.make(config['env-name'], **config.get('env-kwargs', {}))
+        env, env_val, env_tst = self.make_env(config)
+        print('env: {0}; {1};'.format(type(env), env))
+        env.close()
+        print('^_^ The End ^_^')
+
+    def make_env(self, config):
+        '''
+        创建股票的强化学习环境
+        '''
+        run_name = "yt1"
+        saves_path = pathlib.Path(config['saves_dir']) / f"simple-{run_name}"
+        saves_path.mkdir(parents=True, exist_ok=True)
+
+        data_path = pathlib.Path(config['stocks'])
+        val_path = pathlib.Path(config['val_stocks'])
+        year = 2016
+
+        if year is not None or data_path.is_file():
+            if year is not None:
+                print('load stock data...')
+                stock_data = BarData.load_year_data(year)
+            else:
+                stock_data = {"YNDX": BarData.load_relative(data_path)}
+            env = MinuteBarEnv(
+                stock_data, bars_count=config['bars_count'], volumes=True)
+            env_tst = MinuteBarEnv(
+                stock_data, bars_count=config['bars_count'], volumes=True)
+        elif data_path.is_dir():
+            env = MinuteBarEnv.from_dir(
+                data_path, bars_count=config['bars_count'])
+            env_tst = MinuteBarEnv.from_dir(
+                data_path, bars_count=config['bars_count'])
+        else:
+            raise RuntimeError("No data to train on")
+
+        env = gym.wrappers.TimeLimit(env, max_episode_steps=1000)
+        val_data = {"YNDX": BarData.load_relative(val_path)}
+        env_val = MinuteBarEnv(val_data, bars_count=config['bars_count'], volumes=True)
+        return env, env_val, env_tst
+        #return rll.common.wrappers.wrap_dqn(gym.make(AppConfig.a3c_config['ENV_NAME']))

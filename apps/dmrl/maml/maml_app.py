ee93a4a1c263a643552ee53d68be7fd4bb4791d2
yantao <yt7589@qq.com>
2021-07-20 16:50:36 +0800

    v0.0.2: stock quotation maml framework yantao

--- apps/dmrl/maml/maml_app.py
+++ apps/dmrl/maml/maml_app.py
@@ -17,6 +17,7 @@ class MamlApp(object):
         self.chpt_file = './work/maml.pkl'
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.fixed_weights = [] # 需要固定的权值名称列表
+        self.in_size = 50 # 每个行情包括之前10天（包括今天），每天有开盘、最高、最低、收盘、交易量这5个值
 
     def startup(self):
         print('MAML算法 v0.0.2')
@@ -35,7 +36,6 @@ class MamlApp(object):
         stock_symbols = ['sh600260', 'sh600487', 'sh600728']
         target_stock = 'sh600260'
         torch.cuda.set_device(0)
-        in_size = 50 # 每个行情包括之前10天（包括今天），每天有开盘、最高、最低、收盘、交易量这5个值
         n_way = 3
         k_shot = 16
         q_query = 4
@@ -78,7 +78,7 @@ class MamlApp(object):
         train_iter = iter(train_loader)
         val_iter = iter(val_loader)
         test_iter = iter(test_ds)
-        meta_model = MamlModel(in_size, n_way).to(self.device)
+        meta_model = MamlModel(self.in_size, n_way).to(self.device)
         optimizer = torch.optim.Adam(meta_model.parameters(), lr = meta_lr)
         loss_fn = nn.CrossEntropyLoss().to(self.device)
         for epoch in range(max_epoch):
@@ -117,163 +117,33 @@ class MamlApp(object):
             print("  Validation accuracy: ", np.mean(val_acc))
         print('train is OK!')
         torch.save(meta_model.state_dict(), self.chpt_file)
+        self.evaluate_on_test_ds(test_loader, test_iter)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-    def train_org(self):
-        torch.cuda.set_device(0)
-        n_way = 5
-        k_shot = 1
-        q_query = 1
-        inner_train_steps = 1
-        inner_lr = 0.4
+    def evaluate_on_test_ds(self, test_loader, test_iter):
+        n_way = 3
+        k_shot = 16
+        q_query = 4
+        test_batches = 4
         meta_lr = 0.001
-        meta_batch_size = 8 #32
-        max_epoch = 5 #40
-        eval_batches = 20
-        train_data_path = './data/Omniglot/images_background/'
-        dataset = OmniglotDs(train_data_path, n_way, k_shot, q_query)
-        train_set, val_set = torch.utils.data.random_split(
-                    dataset, [3200,656])
-        train_loader = DataLoader(train_set,
-                                batch_size = n_way, # 這裡的 batch size 並不是 meta batch size, 而是一個 task裡面會有多少不同的
-                                                    # characters，也就是 few-shot classifiecation 的 n_way
-                                num_workers = 0, #4,
-                                shuffle = True,
-                                drop_last = True)
-        val_loader = DataLoader(val_set,
-                                batch_size = n_way,
-                                num_workers = 0, #4,
-                                shuffle = True,
-                                drop_last = True)
-        train_iter = iter(train_loader)
-        val_iter = iter(val_loader)
-        meta_model = MamlModel(1, n_way).to(self.device)
-        optimizer = torch.optim.Adam(meta_model.parameters(), lr = meta_lr)
-        loss_fn = nn.CrossEntropyLoss().to(self.device)
-        for epoch in range(max_epoch):
-            print("Epoch %d" %(epoch))
-            train_meta_loss = []
-            train_acc = []
-            for step in tqdm(range(len(train_loader) // 
-                        (meta_batch_size))): # 這裡的 step 是一次 meta-gradinet update step
-                x, y, train_iter = self.get_meta_batch(
-                    meta_batch_size, k_shot, q_query, 
-                    train_loader, train_iter
-                )
-                meta_loss, acc = self.train_batch(
-                    meta_model, optimizer, x, y, n_way, 
-                    k_shot, q_query, loss_fn
-                )
-                train_meta_loss.append(meta_loss.item())
-                train_acc.append(acc)
-            print("  Loss    : ", np.mean(train_meta_loss))
-            print("  Accuracy: ", np.mean(train_acc))
-            # 每個 epoch 結束後，看看 validation accuracy 如何  
-            # 助教並沒有做 early stopping，同學如果覺得有需要是可以做的 
-            val_acc = []
-            for eval_step in tqdm(range(len(val_loader) // 
-                        (eval_batches))):
-                x, y, val_iter = self.get_meta_batch(
-                    eval_batches, k_shot, q_query, 
-                    val_loader, val_iter
-                )
-                _, acc = self.train_batch(
-                    meta_model, optimizer, x, y, n_way, 
-                    k_shot, q_query, loss_fn, 
-                    inner_train_steps = 3, train = False
-                ) # testing時，我們更新三次 inner-step
-                val_acc.append(acc)
-            print("  Validation accuracy: ", np.mean(val_acc))
-        print('train is OK!')
-        torch.save(meta_model.state_dict(), self.chpt_file)
-
-
-    def evaluate_on_test_ds(self):
-        n_way = 5
-        test_data_path = './data/Omniglot/images_evaluation/'
-        k_shot = 1
-        q_query = 1
-        test_batches = 20
-        meta_lr = 0.001
+        '''
         test_loader = DataLoader(OmniglotDs(test_data_path, n_way, k_shot, q_query),
                                 batch_size = n_way,
                                 num_workers = 8,
                                 shuffle = True,
                                 drop_last = True)
         test_iter = iter(test_loader)
+        '''
         test_acc = []
-
-        meta_model = MamlModel(1, n_way).to(self.device)
+        meta_model = MamlModel(self.in_size, n_way).to(self.device)
         meta_model.load_state_dict(torch.load(self.chpt_file))
         optimizer = torch.optim.Adam(meta_model.parameters(), lr = meta_lr)
         loss_fn = nn.CrossEntropyLoss().to(self.device)
-
         for test_step in tqdm(range(len(test_loader) // (test_batches))):
             x, y, val_iter = self.get_meta_batch(test_batches, k_shot, q_query, test_loader, test_iter)
             _, acc = self.train_batch(meta_model, optimizer, x, y, n_way, k_shot, q_query, loss_fn, inner_train_steps = 3, train = False) # testing時，我們更新三次 inner-step
             test_acc.append(acc)
         print("  Testing accuracy: ", np.mean(test_acc))
-
-    def exp(self):
-        print('MAML算法试验代码')
-
-    def exp002(self):
-        '''
-        从文件中读出数据集
-        '''
-        stock1_symbol = 'sh600260'
-        ds = AksDs()
-        X1 = ds.load_X_from_csv(stock1_symbol)
-        y1 = ds.load_y_from_csv(stock1_symbol)
-        print('X1: {0};'.format(X1.shape))
-        print('y1: {0}; dtype={1};'.format(y1.shape, y1.dtype))
-        ###############################################################################
-        #################### 程序结束标志 #######################################
-        ###############################################################################
-        print('^_^ The End ^_^')
-
-    def exp001(self):
-        '''
-        以收盘价为例，画实时折线图
-        '''
-        stock1_symbol = 'sh600260'
-        ds = AksDs()
-        Xs = []
-        ys = []
-        X1, y1 = ds.generate_stock_ds(stock1_symbol, draw_line=True)
-        Xs.append(X1)
-        ys.append(y1)
-        ###############################################################################
-        #################### 程序结束标志 #######################################
-        ###############################################################################
-        print('^_^ The End ^_^')   
-
-
-
-
-
-
-
-
-
-
-    def create_label(self, n_way, k_shot):
-        return torch.arange(n_way).repeat_interleave(k_shot).long()
 
     def train_batch(self, model, optimizer, x, y, n_way, k_shot, 
                 q_query, loss_fn, inner_train_steps= 1, 
@@ -326,12 +196,90 @@ class MamlApp(object):
             except StopIteration:
                 iterator = iter(data_loader)
                 task_data, y = iterator.next()
-            train_data = task_data[:, :k_shot].reshape(-1, 50)
-            val_data = task_data[:, k_shot:].reshape(-1, 50)
+            train_data = task_data[:, :k_shot].reshape(-1, self.in_size)
+            val_data = task_data[:, k_shot:].reshape(-1, self.in_size)
             task_data = torch.cat((train_data, val_data), 0)
             data.append(task_data)
-            print('train_data:{0}; val_data:{1}; task_data:{2}; data:{3};'.format(train_data.shape, val_data.shape, task_data.shape, len(data)))
         return torch.stack(data).float().to(self.device), y, iterator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def exp(self):
+        print('MAML算法试验代码')
+
+    def exp002(self):
+        '''
+        从文件中读出数据集
+        '''
+        stock1_symbol = 'sh600260'
+        ds = AksDs()
+        X1 = ds.load_X_from_csv(stock1_symbol)
+        y1 = ds.load_y_from_csv(stock1_symbol)
+        print('X1: {0};'.format(X1.shape))
+        print('y1: {0}; dtype={1};'.format(y1.shape, y1.dtype))
+        ###############################################################################
+        #################### 程序结束标志 #######################################
+        ###############################################################################
+        print('^_^ The End ^_^')
+
+    def exp001(self):
+        '''
+        以收盘价为例，画实时折线图
+        '''
+        stock1_symbol = 'sh600260'
+        ds = AksDs()
+        Xs = []
+        ys = []
+        X1, y1 = ds.generate_stock_ds(stock1_symbol, draw_line=True)
+        Xs.append(X1)
+        ys.append(y1)
+        ###############################################################################
+        #################### 程序结束标志 #######################################
+        ###############################################################################
+        print('^_^ The End ^_^')   
+
+
+
+
+
+
+
+
+
+
+    def create_label(self, n_way, k_shot):
+        return torch.arange(n_way).repeat_interleave(k_shot).long()
 
     
 
@@ -390,68 +338,3 @@ class MamlApp(object):
             self.fixed_weights.append('conv4.0.bias')
             self.fixed_weights.append('conv4.1.weight')
             self.fixed_weights.append('conv4.1.bias')
-
-
-
-
-
-    
-
-    def train_batch_org(self, model, optimizer, x, y, n_way, k_shot, 
-                q_query, loss_fn, inner_train_steps= 1, 
-                inner_lr = 0.4, train = True):
-        """
-        Args:
-        x is the input omniglot images for a meta_step, shape = [batch_size, n_way * (k_shot + q_query), 1, 28, 28]
-        n_way: 每個分類的 task 要有幾個 class
-        k_shot: 每個類別在 training 的時候會有多少張照片
-        q_query: 在 testing 時，每個類別會用多少張照片 update
-        """
-        criterion = loss_fn
-        task_loss = [] # 這裡面之後會放入每個 task 的 loss 
-        task_acc = []  # 這裡面之後會放入每個 task 的 loss 
-        for meta_batch, meta_batch_y in zip(x, y):
-            train_set = meta_batch[:n_way*k_shot] # train_set 是我們拿來 update inner loop 參數的 data
-            val_set = meta_batch[n_way*k_shot:]   # val_set 是我們拿來 update outer loop 參數的 data
-            fast_weights = OrderedDict(model.named_parameters()) # 在 inner loop update 參數時，我們不能動到實際參數，因此用 fast_weights 來儲存新的參數 θ'
-            for inner_step in range(inner_train_steps): # 這個 for loop 是 Algorithm2 的 line 7~8
-                                                # 實際上我們 inner loop 只有 update 一次 gradients，不過某些 task 可能會需要多次 update inner loop 的 θ'，
-                                                # 所以我們還是用 for loop 來寫
-                train_label = self.create_label(n_way, k_shot).to(self.device)
-                logits = model.functional_forward(train_set, fast_weights)
-                loss = criterion(logits, train_label)
-                grads = torch.autograd.grad(loss, fast_weights.values(), create_graph = True) # 這裡是要計算出 loss 對 θ 的微分 (∇loss) 
-                #fast_weights = OrderedDict((name, param - inner_lr * grad)
-                #                  for ((name, param), grad) in zip(fast_weights.items(), grads)) # 這裡是用剛剛算出的 ∇loss 來 update θ 變成 θ'
-                fast_weights = OrderedDict((name, param) if name in self.fixed_weights else (name, param - inner_lr * grad) for ((name, param), grad) in zip(fast_weights.items(), grads))
-            val_label = self.create_label(n_way, q_query).to(self.device)
-            logits = model.functional_forward(val_set, fast_weights) # 這裡用 val_set 和 θ' 算 logit
-            loss = criterion(logits, val_label)                      # 這裡用 val_set 和 θ' 算 loss
-            task_loss.append(loss)                                   # 把這個 task 的 loss 丟進 task_loss 裡面
-            acc = np.asarray([torch.argmax(logits, -1).cpu().numpy() == val_label.cpu().numpy()]).mean() # 算 accuracy
-            task_acc.append(acc)
-        model.train()
-        optimizer.zero_grad()
-        meta_batch_loss = torch.stack(task_loss).mean() # 我們要用一整個 batch 的 loss 來 update θ (不是 θ')
-        if train:
-            meta_batch_loss.backward()
-            optimizer.step()
-        task_acc = np.mean(task_acc)
-        return meta_batch_loss, task_acc
-
-    def get_meta_batch_org(self, meta_batch_size, k_shot, q_query, data_loader, iterator):
-        data = []
-        y = torch.tensor([])
-        for _ in range(meta_batch_size):
-            try:
-                task_data, y = iterator.next()  # 一筆 task_data 就是一個 task 裡面的 data，大小是 [n_way, k_shot+q_query, 1, 28, 28]
-            except StopIteration:
-                iterator = iter(data_loader)
-                task_data, y = iterator.next()
-            print('task_data: {0}={1};'.format(type(task_data), task_data.shape))
-            print('y: {0}={1};'.format(type(y), y.shape))
-            train_data = task_data[:, :k_shot].reshape(-1, 1, 28, 28)
-            val_data = task_data[:, k_shot:].reshape(-1, 1, 28, 28)
-            task_data = torch.cat((train_data, val_data), 0)
-            data.append(task_data)
-        return torch.stack(data).to(self.device), y, iterator
--- apps/dmrl/maml/maml_model.py
+++ apps/dmrl/maml/maml_model.py
@@ -3,25 +3,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def ConvBlock(in_ch, out_ch):
-    return nn.Sequential(nn.Conv2d(in_ch, out_ch, 3, padding = 1),
-                        nn.BatchNorm2d(out_ch),
-                        nn.ReLU(),
-                        nn.MaxPool2d(kernel_size = 2, stride = 2)) # 原作者在 paper 裡是說她在 omniglot 用的是 strided convolution
-                                                                    # 不過這裡我改成 max pool (mini imagenet 才是 max pool)
-                                                                    # 這並不是你們在 report 第三題要找的 tip
-
-def ConvBlockFunction(x, w, b, w_bn, b_bn):
-    x = F.conv2d(x, w, b, padding = 1)
-    x = F.batch_norm(x, running_mean = None, running_var = None, 
-                weight = w_bn, bias = b_bn, training = True)
-    x = F.relu(x)
-    x = F.max_pool2d(x, kernel_size = 2, stride = 2)
-    return x
-
-
-
-
 def LinearBlock(in_size, out_size):
     return nn.Sequential(
         nn.Linear(in_size, out_size),
@@ -36,31 +17,15 @@ def LinearBlockFunction(x, w, b, w_bn, b_bn):
     x = F.relu(x)
     return x
 
-
-
-
-
 class MamlModel(nn.Module):
     def __init__(self, in_size, n_way):
         super(MamlModel, self).__init__()
-        '''
-        self.conv1 = ConvBlock(in_ch, 64)
-        self.conv2 = ConvBlock(64, 64)
-        self.conv3 = ConvBlock(64, 64)
-        self.conv4 = ConvBlock(64, 64)
-        '''
         self.linear1 = LinearBlock(in_size, 64)
-        self.linear2 = LinearBlock(64, 64)
-        self.linear3 = LinearBlock(64, 64)
-        self.logits = nn.Linear(64, n_way)
+        self.linear2 = LinearBlock(64, 32)
+        self.linear3 = LinearBlock(32, 16)
+        self.logits = nn.Linear(16, n_way)
     
     def forward(self, x):
-        '''
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        '''
         x = self.linear1(x)
         x = self.linear2(x)
         x = self.linear3(x)
@@ -75,14 +40,6 @@ class MamlModel(nn.Module):
         params: 模型的參數，也就是 convolution 的 weight 跟 bias，
                 以及 batchnormalization 的  weight 跟 bias
                 這是一個 OrderedDict
-        '''
-
-        '''
-        for block in [1, 2, 3, 4]:
-            x = ConvBlockFunction(x, params[f'conv{block}.0.weight'], 
-                        params[f'conv{block}.0.bias'],                                 
-                        params.get(f'conv{block}.1.weight'), 
-                        params.get(f'conv{block}.1.bias'))
         '''
         for block in [1, 2, 3]:
             x = LinearBlockFunction(x,

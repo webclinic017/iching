765dec3b03af4aa62b7710243b8cb7e4433e455a
yantao <yt7589@qq.com>
2021-08-02 16:33:04 +0800

    add iqtt transformer implementation

--- biz/dmrl/iqtt/iqtt_app.py
+++ biz/dmrl/iqtt/iqtt_app.py
@@ -1,4 +1,5 @@
 # 
+from argparse import ArgumentParser
 import torch
 from biz.dmrl.iqtt.self_attention import SelfAttention
 
@@ -8,6 +9,73 @@ class IqttApp(object):
 
     def startup(self, args={}):
         print('Iching Quantitative Trading Transformer v0.0.1')
+        cmd_args = self.parse_args()
+        print('command line args: {0};'.format(cmd_args))
+
+    def parse_args(self):
+        parser = ArgumentParser()
+        parser.add_argument("-e", "--num-epochs",
+                        dest="num_epochs",
+                        help="Number of epochs.",
+                        default=80, type=int)
+        parser.add_argument("-b", "--batch-size",
+                        dest="batch_size",
+                        help="The batch size.",
+                        default=4, type=int)
+        parser.add_argument("-l", "--learn-rate",
+                        dest="lr",
+                        help="Learning rate",
+                        default=0.0001, type=float)
+        parser.add_argument("-T", "--tb_dir", dest="tb_dir",
+                        help="Tensorboard logging directory",
+                        default='./runs')
+        parser.add_argument("-f", "--final", dest="final",
+                        help="Whether to run on the real test set (if not included, the validation set is used).",
+                        action="store_true")
+        parser.add_argument("--max-pool", dest="max_pool",
+                        help="Use max pooling in the final classification layer.",
+                        action="store_true")
+        parser.add_argument("-E", "--embedding", dest="embedding_size",
+                        help="Size of the character embeddings.",
+                        default=128, type=int)
+        parser.add_argument("-V", "--vocab-size", dest="vocab_size",
+                        help="Number of words in the vocabulary.",
+                        default=50_000, type=int)
+        parser.add_argument("-M", "--max", dest="max_length",
+                        help="Max sequence length. Longer sequences are clipped (-1 for no limit).",
+                        default=512, type=int)
+        parser.add_argument("-H", "--heads", dest="num_heads",
+                        help="Number of attention heads.",
+                        default=8, type=int)
+        parser.add_argument("-d", "--depth", dest="depth",
+                        help="Depth of the network (nr. of self-attention layers)",
+                        default=6, type=int)
+        parser.add_argument("-r", "--random-seed",
+                        dest="seed",
+                        help="RNG seed. Negative for random",
+                        default=1, type=int)
+        parser.add_argument("--lr-warmup",
+                        dest="lr_warmup",
+                        help="Learning rate warmup.",
+                        default=10_000, type=int)
+        parser.add_argument("--gradient-clipping",
+                        dest="gradient_clipping",
+                        help="Gradient clipping.",
+                        default=1.0, type=float)
+        return parser.parse_args()
+
+
+
+
+
+
+
+
+
+
+
+
+    def t1(self):
         sa = SelfAttention(emb=5, heads=2, mask=False)
         x = torch.rand(8, 12, 5)
         y = sa(x)

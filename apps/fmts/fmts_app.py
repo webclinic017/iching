13f0befdea345966f977c7f8ea3320dac932c55c
yantao <yt7589@qq.com>
2021-08-30 17:41:53 +0800

    add self attention class

--- apps/fmts/fmts_app.py
+++ apps/fmts/fmts_app.py
@@ -6,6 +6,6 @@ class FmtsApp(object):
         self.name = 'apps.fmts.fmts_app.FmtsApp'
 
     def startup(self, args={}):
-        print('金融市场交易系统 v0.0.1')
+        print('金融市场交易系统 v0.0.2')
         print('run_mode={0}; num_epoch={1};'.format(args['run_mode'], args['num_epochs']))
         print('config version: {0};'.format(AppConfig.version))
